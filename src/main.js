cc22927f10ef4c6d8dde20d321aa6561f7bc2d36
Eduard Maura <edu.maura.puig@gmail.com>
2018-11-14 12:57:11 +0100

    Add sounds and js animations to play sound when key is pressed

--- src/main.js
+++ src/main.js
@@ -8,23 +8,32 @@ const KEY_J = 74;
 const KEY_K = 75;
 const KEY_L = 76;
 const keyList = [KEY_A, KEY_S, KEY_D, KEY_F, KEY_G, KEY_H, KEY_J, KEY_K, KEY_L];
-const keyMap = { [KEY_A]: "A", [KEY_S]: "S", [KEY_D]: "D", [KEY_F]: "F", [KEY_G]: "G", [KEY_H]: "H",[ KEY_J]: "J", [KEY_K]: "K", [KEY_L]: "L" };
+const keyMap = { [KEY_A]: 'A', [KEY_S]: 'S', [KEY_D]: 'D', [KEY_F]: 'F', [KEY_G]: 'G', [KEY_H]: 'H', [KEY_J]: 'J', [KEY_K]: 'K', [KEY_L]: 'L' };
 
-(function () {
+document.addEventListener('keydown', function (event) {
+  var keyPressed = event.keyCode;
+  if (!keyList.includes(keyPressed)) return
+  addPressedAnimation(keyPressed);
+  playSound(keyPressed);
+});
 
-  document.addEventListener("keydown", function (event) {
-    var keyPressed = event.keyCode;
-    if (keyList.includes(keyPressed)) {
-      var div = document.getElementById(keyMap[keyPressed]);
-      div.classList.add('press');
-    }
-  });
+document.addEventListener('keyup', function (event) {
+  var keyPressed = event.keyCode;
+  if (!keyList.includes(keyPressed)) return;
+  removePressAnimation(keyPressed);
+});
 
-  document.addEventListener("keyup", function (event) {
-    var keyPressed = event.keyCode;
-    if (keyList.includes(keyPressed)) {
-      var div = document.getElementById(keyMap[keyPressed]);
-      div.classList.remove('press');
-    }
-  });
-})();
+function playSound(keyPressed) {
+  var sound = document.getElementById('audio-' + keyMap[keyPressed]);
+  sound.play();
+}
+
+function addPressedAnimation(keyPressed) {
+  var div = document.getElementById(keyMap[keyPressed]);
+  div.classList.add('press');
+}
+
+function removePressAnimation(keyPressed) {
+  var div = document.getElementById(keyMap[keyPressed]);
+  div.classList.remove('press');
+}
