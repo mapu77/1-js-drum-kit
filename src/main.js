51e31d67421d83dd99201ccbc74aedca17374a95
Eduard Maura <edu.maura.puig@gmail.com>
2018-11-26 19:07:01 +0100

    Add test framework and refactor JS to be testeable

--- src/main.js
+++ src/main.js
@@ -1,40 +1,35 @@
-const KEY_A = 65;
-const KEY_S = 83;
-const KEY_D = 68;
-const KEY_F = 70;
-const KEY_G = 71;
-const KEY_H = 72;
-const KEY_J = 74;
-const KEY_K = 75;
-const KEY_L = 76;
-const keyList = [KEY_A, KEY_S, KEY_D, KEY_F, KEY_G, KEY_H, KEY_J, KEY_K, KEY_L];
-const keyMap = { [KEY_A]: 'A', [KEY_S]: 'S', [KEY_D]: 'D', [KEY_F]: 'F', [KEY_G]: 'G', [KEY_H]: 'H', [KEY_J]: 'J', [KEY_K]: 'K', [KEY_L]: 'L' };
+const validKeys = ['KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL'];
 
 document.addEventListener('keydown', function (event) {
-  var keyPressed = event.keyCode;
-  if (!keyList.includes(keyPressed)) return
-  addPressedAnimation(keyPressed);
-  playSound(keyPressed);
+    if (!isAValidKey(event.code)) return;
+    addPressedAnimation(event.key);
+    playSound(event.key);
 });
 
 document.addEventListener('keyup', function (event) {
-  var keyPressed = event.keyCode;
-  if (!keyList.includes(keyPressed)) return;
-  removePressAnimation(keyPressed);
+    if (!isAValidKey(event.code)) return;
+    removePressAnimation(event.key);
 });
 
-function playSound(keyPressed) {
-  var sound = document.getElementById('audio-' + keyMap[keyPressed]);
-  sound.currentTime = 0;
-  sound.play();
-}
 
 function addPressedAnimation(keyPressed) {
-  var div = document.getElementById(keyMap[keyPressed]);
-  div.classList.add('press');
+    let div = document.getElementById(keyPressed);
+    div.classList.add('press');
 }
 
 function removePressAnimation(keyPressed) {
-  var div = document.getElementById(keyMap[keyPressed]);
-  div.classList.remove('press');
+    let div = document.getElementById(keyPressed);
+    div.classList.remove('press');
 }
+
+function playSound(keyPressed) {
+    let sound = document.getElementById('audio-' + keyPressed);
+    sound.currentTime = 0;
+    sound.play();
+}
+
+function isAValidKey(keyPressed) {
+    return validKeys.includes(keyPressed);
+}
+
+module.exports = {addPressedAnimation, removePressAnimation, isAValidKey};
